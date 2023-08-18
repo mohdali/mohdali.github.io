06af9b9f980f81935c389f8c3322332acff61870
Mohamed Ali <antivirus.cs@gmail.com>
2023-08-18 10:31:28 +0300

    restructure project

--- pre-render.ps1
+++ pre-render.ps1
@@ -3,13 +3,18 @@ If(Test-Path .\Prerender\output)
     Remove-Item -Path .\Prerender\output -Recurse
 }
  
-dotnet publish -c Release -o Prerender/output --nologo
+dotnet publish src/mohdali.github.io/mohdali.github.io.csproj -c Release -o Prerender/output --nologo
+
 Push-Location .\Prerender
+
 npx react-snap
+
 Get-ChildItem ".\output\wwwroot\*.html" -Recurse | ForEach-Object { 
     $HtmlFileContent = (Get-Content -Path $_.FullName -Raw);
     $HtmlFileContent = $HtmlFileContent.Replace('<script>var Module; window.__wasmmodulecallback__(); delete window.__wasmmodulecallback__;</script><script src="_framework/dotnet.6.0.7.8zwu4egow5.js" defer="" integrity="sha256-pvJcJ6jtDSfxgXm03QYRTm4aAi9PSzMSJODReHtvASY=" crossorigin="anonymous"></script>','')
     Set-Content -Path $_.FullName -Value $HtmlFileContent
 }
+
 Pop-Location
+
 dotnet serve -o -dprerender/output/wwwroot/
