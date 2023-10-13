fe418ff5b322accde1c74da896447adb97d2d10c
Mohamed Ali <antivirus.cs@gmail.com>
2023-10-13 13:01:58 +0300

    Add Theme switcher

---
+++ src/mohdali.github.io/wwwroot/js/app.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const ThemeOption = {
+    System: "System",
+    Dark: "Dark",
+    Light: "Light"
+}
+
+window.getTheme = () => localStorage.getItem('theme');
+
+window.storeTheme = theme => localStorage.setItem('theme', theme);
+
+window.getThemePreference = () => {
+    const theme = getTheme()
+
+    if (theme && theme != ThemeOption.System)
+        return theme;
+
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'Dark' : 'Light'
+}
+
+window.setTheme = () => {
+    const theme = getThemePreference();
+
+    if (theme == ThemeOption.Dark) {
+        document.body.classList.add(
+            'dark-theme'
+        );
+    }
+    else {
+        document.body.classList.remove(
+            'dark-theme'
+        );
+    }
+}
+
+window.onload = (event) => {
+    console.log("Hello World!");
+    setTheme();
+};
