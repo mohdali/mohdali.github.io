2c5f069e3416e13827bd27ecce626e6e82bcbaec
Mohamed Ali <antivirus.cs@gmail.com>
2023-11-25 19:14:46 +0300

    necessary plumbing to make it work finally!

--- src/BlogEngine.Markdown/MarkdownGenerator.cs
+++ src/BlogEngine.Markdown/MarkdownGenerator.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Markdig;
+using Markdig.Extensions.Yaml;
+using Markdig.Renderers;
+using Markdig.Syntax;
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.CodeAnalysis;
+using YamlDotNet.Serialization;
 
 namespace BlogEngine.Markdown
 {
@@ -22,8 +28,9 @@ namespace BlogEngine.Markdown
                 rootNamespace = "MD";
             }
 
-            var projectEngine = RazorProjectEngine.Create(RazorConfiguration.Default, fs, (b) => {
-                b.SetRootNamespace(rootNamespace);                
+            var projectEngine = RazorProjectEngine.Create(RazorConfiguration.Default, fs, (b) =>
+            {
+                b.SetRootNamespace(rootNamespace);
             });
 
             foreach (var file in context.AdditionalFiles)
@@ -40,7 +47,7 @@ namespace BlogEngine.Markdown
 
                     var generatedCode = csharpDocument.GeneratedCode;
 
-                    context.AddSource($"CSharp_{Path.GetFileNameWithoutExtension(file.Path)}.g.cs", generatedCode);
+                    context.AddSource($"{Path.GetFileNameWithoutExtension(file.Path)}.g.cs", generatedCode);
                 }
             }
         }
@@ -62,18 +69,52 @@ namespace BlogEngine.Markdown
 
             public override string FileKind => _item.FileKind;
 
+            public Dictionary<string, string> Frontmatter { get; private set; }
+
+            public string Htmlstring { get; private set; }
+
             public override Stream Read()
             {
-                using(var reader = new StreamReader(_item.Read()))
+                using (var reader = new StreamReader(_item.Read()))
                 {
-                    var piepline = new MarkdownPipelineBuilder()
+                    var pipeline = new MarkdownPipelineBuilder()
+                        .UseYamlFrontMatter()
                         .Build();
+
+                    var writer = new StringWriter();
+
+                    var renderer = new HtmlRenderer(writer);
+
+                    pipeline.Setup(renderer);
 
                     var stringContent = reader.ReadToEnd();
 
-                    var htmlString = Markdig.Markdown.ToHtml(stringContent, piepline);
+                    var document = Markdig.Markdown.Parse(stringContent, pipeline);
 
-                    var outputStream = new MemoryStream(Encoding.UTF8.GetBytes(htmlString));
+                    var yamlBlock = document.Descendants<YamlFrontMatterBlock>().FirstOrDefault();
+
+                    if(yamlBlock != null)
+                    {
+                        string yaml = stringContent.Substring(yamlBlock.Span.Start , yamlBlock.Span.Length).Replace("---\r\n","").Replace("\r\n---", "");
+                        
+                        var deserializer = new DeserializerBuilder()
+                            .Build();
+
+                        Frontmatter = deserializer.Deserialize<Dictionary<string, string>>(yaml);
+                    }
+
+                    renderer.Render(document);
+
+                    writer.Flush();
+
+                    Htmlstring = writer.ToString();
+
+                    foreach(var kv in Frontmatter)
+                    {
+                        Htmlstring = $"@{kv.Key} \"{kv.Value}\"\r\n{Htmlstring}";
+                    }
+                    
+                    var outputStream = new MemoryStream(Encoding.UTF8.GetBytes(Htmlstring));
 
                     return outputStream;
                 }
