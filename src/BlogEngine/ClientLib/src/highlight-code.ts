116a0435cb24a4bbf0d913fad7978b9e620550d3
Mohamed Ali <antivirus.cs@gmail.com>
2024-03-31 12:49:35 +0300

    disable copy plugin

--- src/BlogEngine/ClientLib/src/highlight-code.ts
+++ src/BlogEngine/ClientLib/src/highlight-code.ts
@@ -4,8 +4,9 @@ import '../node_modules/highlight.js/styles/dark.css'
 import '../node_modules/highlightjs-copy/dist/highlightjs-copy.min.css'
 
 export function highlightCode() {
+    //hljs.addPlugin(CopyButtonPlugin);
+
     document.querySelectorAll('pre code').forEach((el : HTMLElement) => {
         hljs.highlightElement(el);
     });
-    hljs.addPlugin(new CopyButtonPlugin());
 }
